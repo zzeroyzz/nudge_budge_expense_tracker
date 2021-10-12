d0df82bba3a09dc01b53650cc2bbd39fbe399bfe
kato hawkins <kristhahawkins@icloud.com>
2021-10-12 19:28:50 -0400

    imported Main component

--- nudge_budge_expense_tracker/src/App.js
+++ nudge_budge_expense_tracker/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Grid} from '@material-ui/core'
 
 import Details from './components/details/Details'
+import Main from './components/Main/Main'
 import useStyles from './appstyles'
 const App = () => {
     const classes = useStyles();
@@ -12,7 +13,7 @@ const App = () => {
                     <Details title="Income"/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    Main
+                    <Main />
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <Details title="Expense" />
