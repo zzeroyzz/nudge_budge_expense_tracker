b0ebbdb2795cadbef08903b70242e2e2e7feca1c
kato hawkins <kristhahawkins@icloud.com>
2021-10-12 18:47:13 -0400

    changed name from useStyles to Makestyle

--- nudge_budge_expense_tracker/src/components/details/styles.js
+++ nudge_budge_expense_tracker/src/components/details/styles.js
@@ -1,6 +1,6 @@
 import {makeStyles} from '@material-ui/core'
 
-export default useStyles(() =>({
+export default makeStyles(() =>({
     income:{
         borderBottom:'10px solid rgba(0, 255, 0, 0.5)',  
     },
