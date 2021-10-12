acab715305454645acfd747d2b65b6cd1503124f
kato hawkins <kristhahawkins@icloud.com>
2021-10-12 18:48:08 -0400

    Added Grid container and added Grid Items

--- nudge_budge_expense_tracker/src/App.js
+++ nudge_budge_expense_tracker/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import {Grid} from '@material-ui/core'
 
+import Details from './components/details/Details'
 
 const App = () => {
     return (
         <div>
-            App
+           <Grid container spacing={0} alignItems="center" style={{height:'100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+           </Grid>
+       
         </div>
     )
 }
