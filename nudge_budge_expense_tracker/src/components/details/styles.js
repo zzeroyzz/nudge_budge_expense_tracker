ab9d42a7c843a62f6940c4d9de11067daa2f5bbd
kato hawkins <kristhahawkins@icloud.com>
2021-10-12 18:39:36 -0400

    created a useStyles to implement styling to Card

--- nudge_budge_expense_tracker/src/components/details/styles.js
+++ nudge_budge_expense_tracker/src/components/details/styles.js
@@ -1,10 +1,10 @@
 import {makeStyles} from '@material-ui/core'
 
-export default makeStyles(() =>({
+export default useStyles(() =>({
     income:{
         borderBottom:'10px solid rgba(0, 255, 0, 0.5)',  
     },
     expense:{
-        borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
+        borderBottom: '10px solid rgba(255, 0, 0, 0.5)',
     }
 }))
