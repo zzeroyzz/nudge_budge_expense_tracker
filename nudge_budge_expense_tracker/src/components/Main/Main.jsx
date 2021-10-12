357eddd703a90d0444768f90463924037c7a7ebf
kato hawkins <kristhahawkins@icloud.com>
2021-10-12 19:56:02 -0400

    Imported Form Component. Added componented with Main Component

--- nudge_budge_expense_tracker/src/components/Main/Main.jsx
+++ nudge_budge_expense_tracker/src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 
 import useStyles from './styles'
-
+import Form from "./Form/Form"
 const Main = () => {
     const classes = useStyles();
 
@@ -16,7 +16,7 @@ const Main = () => {
                     Try saying: Add income for $100 in Category Salary for Monday
                 </Typography>
                 <Divider />
-                {/* {form} */}
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
