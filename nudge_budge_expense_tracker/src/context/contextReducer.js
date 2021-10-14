a18bc4f1b4f6b49fa521bfd379b6f803b6359c2d
kato hawkins <kristhahawkins@icloud.com>
2021-10-14 11:44:40 -0400

    created reducer

--- nudge_budge_expense_tracker/src/context/contextReducer.js
+++ nudge_budge_expense_tracker/src/context/contextReducer.js
@@ -1,8 +1,23 @@
 
-
+const transaction =[
+    {
+        id:1,
+    }
+]
 
 const contextReducer = (state,action) =>{
-    if action
+    let transaction;
+    switch (key) {
+        case 'DELETE_TRANSACTION':
+            const transaction = state.filter((t) => t.id !=== action.payload);
+            return transaction
+            
+        case 'ADD_TRANSACTION':
+            const transaction = [action.payload, ...state]
+            return transaction
+        default:
+            return state;
+    }
 }
 
 export default contextReducer
