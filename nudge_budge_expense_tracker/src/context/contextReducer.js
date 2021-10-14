c337c1e17136ec21ed2c8d1c6adacd1e82601975
kato hawkins <kristhahawkins@icloud.com>
2021-10-14 13:20:54 -0400

    removed the const from the two transaction variables. Set local transaction variable inside the function

--- nudge_budge_expense_tracker/src/context/contextReducer.js
+++ nudge_budge_expense_tracker/src/context/contextReducer.js
@@ -7,13 +7,13 @@ const transaction =[
 
 const contextReducer = (state,action) =>{
     let transaction;
-    switch (key) {
+    switch (action.type) {
         case 'DELETE_TRANSACTION':
-            const transaction = state.filter((t) => t.id !=== action.payload);
+             transaction = state.filter((t) => t.id !== action.payload);
             return transaction
             
         case 'ADD_TRANSACTION':
-            const transaction = [action.payload, ...state]
+             transaction = [action.payload, ...state]
             return transaction
         default:
             return state;
