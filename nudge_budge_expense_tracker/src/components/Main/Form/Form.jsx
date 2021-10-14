647753d16829e7f32176a6436ba8ba0aedaef120
kato hawkins <kristhahawkins@icloud.com>
2021-10-14 18:07:17 -0400

    found error within the createTransaction function. I did not password transcation through addTransaction

--- nudge_budge_expense_tracker/src/components/Main/Form/Form.jsx
+++ nudge_budge_expense_tracker/src/components/Main/Form/Form.jsx
@@ -1,23 +1,24 @@
 import React, {useState, useContext} from 'react'
 import {TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem} from '@material-ui/core'
-import {NudgeBudgeExpenseTrackerContext} from '../../../context/Context'
+import {NudgeBudgeExpenseTrackerContext} from '../../../context/context'
 import useStyles from './styles'
 import {v4 as uuidv4} from 'uuid'
+
 const initialState = {
-    amount:'',
-    category:'',
-    type:'Income',
-    date: new Date(),
-}
+  amount: '',
+  category: '',
+  type: 'Income',
+  date: new Date(),
+};
 
 const Form = () => {
     const classes = useStyles();
-    const [formData, setformData] = useState(initialState)
+    const [formData, setFormData] = useState(initialState)
     const {addTransaction} = useContext(NudgeBudgeExpenseTrackerContext)
     
     const createTransaction = () =>{
         const transaction = { ...formData, amount:Number(formData.amount), id:uuidv4()}
-        addTransaction()
+        addTransaction(transaction)
         setFormData(initialState)
     }
     
@@ -31,26 +32,26 @@ const Form = () => {
             <Grid item xs={6}>
             <FormControl fullWidth>
                 <InputLabel> Type </InputLabel>
-                <Select value={formData.type} onChange={(e) => setformData({...formData, type: e.target.value})}>
-                    <MenuItem value="Income"> Income</MenuItem>
-                    <MenuItem value="Expense"> Expense</MenuItem>
+                <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
+                    <MenuItem value='Income'>Income</MenuItem>
+                    <MenuItem value='Expense'>Expense</MenuItem>
                 </Select>
             </FormControl>
             </Grid>
         <Grid item xs={6}> 
             <FormControl fullWidth>
                 <InputLabel> Category </InputLabel>
-                <Select value={formData.category} onChange={(e) => setformData({...formData, category: e.target.value})}>
+                <Select value={formData.category} onChange={(e) => setFormData({...formData, category: e.target.value})}>
                     <MenuItem value="business">Business </MenuItem>
                     <MenuItem value="salary">Salary</MenuItem>
                 </Select>
             </FormControl>
         </Grid>
         <Grid item xs={6}>
-            <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setformData({...formData, amount:e.target.value}) } />
+            <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({...formData, amount:e.target.value}) } />
         </Grid>
         <Grid item xs={6}>
-            <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setformData({...formData, date:e.target.value}) }/>
+            <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({...formData, date:e.target.value}) }/>
         </Grid>
         <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}> Create </Button>
         </Grid>
