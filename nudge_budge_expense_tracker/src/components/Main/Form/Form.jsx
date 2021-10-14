e9f67f05949417ff9c49c566cf7927bd30f7efac
kato hawkins <kristhahawkins@icloud.com>
2021-10-14 11:53:32 -0400

    created an initialState. Gave the select tags value and implement an onChange function to them

--- nudge_budge_expense_tracker/src/components/Main/Form/Form.jsx
+++ nudge_budge_expense_tracker/src/components/Main/Form/Form.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem} from '@material-ui/core'
 
 import useStyles from './styles'
 
+const initialState = {
+    amount:'',
+    category:'',
+    type:'Income',
+    date: new Date(),
+}
+
 const Form = () => {
     const classes = useStyles();
-
+    const [formData, setformData] = useState(initialState)
 
     return (
         <Grid container spacing={2}>
@@ -17,7 +24,7 @@ const Form = () => {
             <Grid item xs={6}>
             <FormControl fullWidth>
                 <InputLabel> Type </InputLabel>
-                <Select>
+                <Select value={formData.type} onChange={(e) => setFormData({...formData, type: e.target.value})}>
                     <MenuItem value="Income"> Income</MenuItem>
                     <MenuItem value="Expense"> Expense</MenuItem>
                 </Select>
@@ -26,17 +33,17 @@ const Form = () => {
         <Grid item xs={6}> 
             <FormControl fullWidth>
                 <InputLabel> Category </InputLabel>
-                <Select>
+                <Select value={formData.category} onChange={(e) => setFormDate({...formData, category: e.target.value})}>
                     <MenuItem value="business">Business </MenuItem>
                     <MenuItem value="salary">Salary</MenuItem>
                 </Select>
             </FormControl>
         </Grid>
         <Grid item xs={6}>
-            <TextField type="number" label="Amount" fullWidth />
+            <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({...formData, amount:e.target.value}) } />
         </Grid>
         <Grid item xs={6}>
-            <TextField type="date" label="Date" fullWidth />
+            <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({...formData, date:e.target.value}) }/>
         </Grid>
         <Button className={classes.button} variant="outlined" color="primary" fullWidth> Create </Button>
         </Grid>
