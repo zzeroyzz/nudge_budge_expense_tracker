b0fff5ddd98df856b7c9fa8b5df9132a0db60c06
kato hawkins <kristhahawkins@icloud.com>
2021-10-18 14:33:06 -0400

    imported Speechly and wrapped it around application

--- nudge_budge_expense_tracker/src/index.js
+++ nudge_budge_expense_tracker/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
+import {SpeechProvider} from '@speechly/react-client'
 import {Provider} from './context/context'
 
 import App from "./App"
@@ -8,8 +8,10 @@ import './index.css'
 
 
 ReactDom.render(
+    <SpeechProvider appId="4a03afef-de0e-411d-b853-d75e476e5080" language="en-US">
     <Provider>
         <App />
-    </Provider>, 
+    </Provider>
+    </SpeechProvider> ,
     document.getElementById('root')
 );
